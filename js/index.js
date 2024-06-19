f7419999b9c5b3061d0e75fc97324ee3670a32d1
alicewu0210 <alicewu0210@gmail.com>
2024-06-19 12:05:02 -0400

    week 19 homewoek

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$('#doi').mouseover(function () {
+    $('.image-overlay').show();
+}).mouseout(function () {
+    $('.image-overlay').hide();
+});
+
+$('#npo').mouseover(function () {
+    $('.image-overlay-2').show();
+}).mouseout(function () {
+    $('.image-overlay-2').hide();
+});
+
+$('#camping').mouseover(function () {
+    $('.image-overlay-3').show();
+}).mouseout(function () {
+    $('.image-overlay-3').hide();
+});
